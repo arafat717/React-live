f267ed697dad85dbafbb0927e8c5b8b4d31424f9
arafat717 <arafatjibon33@gmail.com>
2024-01-06 16:17:18 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+const App = React.createElement(
+  "h1",
+  { style: { color: "red" } },
+  "Hello World"
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(App);
+
+console.log(root);
