53f6a5c0873726c3b13a7f66aa2480fe0e205e19
arafat717 <arafatjibon33@gmail.com>
2024-01-06 17:05:39 +0600

    added upkg babel

--- main.js
+++ main.js
@@ -4,8 +4,18 @@ const App = React.createElement(
   "Hello World"
 );
 
+// const HelloWorld = () => {
+//   return <h1> thsi is sisisi</h1>;
+// };
+
+// const App = () => {
+//   return (
+//     <React.Fragment>
+//       <div>this is arafat jibon</div>
+//       <HelloWorld></HelloWorld>
+//     </React.Fragment>
+//   );
+// };
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
 root.render(App);
-
-console.log(root);
